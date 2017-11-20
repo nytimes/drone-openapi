6465baaf8ae86ea4ef5a02b76540811f86bbc5ea
JP Robinson <john.paul.robinson@gmail.com>
2017-11-20 18:14:06 -0500

    adding debug stmt

--- main.go
+++ main.go
@@ -152,6 +152,7 @@ func publishSpec(vargs API) error {
 		fmt.Printf("successfully published spec file: %s\n", vargs.Spec)
 		return nil
 	}
+	fmt.Println("failed to post body:\n\n%q\n\n", string(payload))
 	return errors.New("unable to publish specs after 3 attempts")
 }
 
